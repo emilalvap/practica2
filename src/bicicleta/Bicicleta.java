0a2ba3f7d0c9270717dd9eb2ca710c86d139e89a
juancly <juancarlosmarco@gmail.com>
2012-12-05 16:18:25 +0100

    añadido el constructor

--- src/bicicleta/Bicicleta.java
+++ src/bicicleta/Bicicleta.java
@@ -5,15 +5,23 @@ public class Bicicleta
 	
 	//pruebagit
 	//el numero actual del piñon y plato activos
+	
 	protected int pinonact = 1;
 	protected int platoact = 1;
 	
-	//los radios que tiene cada plato y piñon
-	protected float dientesplato [];
-	protected float dientespinon [];
+	//los dientes que tiene cada plato y piñon
+	protected int dientesplato [];
+	protected int dientespinon [];
 	
 	//este metodo cambia el piñon, donde piñonact , es el piñon actual usado, accion 
 	// es si queremos aumentar o disminuir este y numpiñon es el piñon maximo
+	
+	public void Bicicleta(int numeropinones, int numeroplatos)
+	{
+		
+		dientesplato  = new int[numeroplatos];
+		dientespinon  = new int[numeropinones];
+	}
 	public void setPinon(char accion)
 	{
 		//aumentamos el piñon
