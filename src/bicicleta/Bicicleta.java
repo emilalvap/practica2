41da946e22b8d7884eaa1cdfd446d76d2348eb56
juancly <juancarlosmarco@gmail.com>
2012-12-05 19:06:31 +0100

    añadido el metodo setNumeroDientes

--- src/bicicleta/Bicicleta.java
+++ src/bicicleta/Bicicleta.java
@@ -25,7 +25,10 @@ public class Bicicleta extends Vehiculo
 	
 		dientesplato  = new int[numeroplatos];
 		dientespinon  = new int[numeropinones];
+		numruedas = 2;
 	}
+	
+	
 	
 	
 	
@@ -42,6 +45,45 @@ public class Bicicleta extends Vehiculo
 	public int getPlato()
 	{
 		return pinonact;
+	}
+	
+	/**
+	 * asigna el numero de dientes correspondiente al piñon o plato,
+	 * 
+	 * @param numerodientes es el array que contiene el numero de dientes por piñon o plato que 
+	 * queremos asignar
+	 * 
+	 * @param array sirve para introducir o bien el array de platos o el array de piñones al
+	 *  cual se le va a asignar el numero de dientes
+	 */
+	public void setAsignaNumeroDientes(int [] numerodientes, int array[])
+	{
+		int i = 0;
+		boolean compruebadientes = true;
+		if (numerodientes.length == array.length)
+		{	
+			
+			while(i< numerodientes.length && compruebadientes)
+			{
+			
+				array[i] = numerodientes[i];
+				if( numerodientes[i]<= numerodientes[i+1] )
+				{
+					compruebadientes = false;
+				}
+				i++;
+			}
+			if ( !compruebadientes)
+			{
+				System.out.println("Los dientes no se asignaron correctamente porque el "+
+						"disco siguiente tenia mas dientes que el anterior");
+			}
+		}
+		else
+		{
+			System.out.print("no se pueden asignar dientes porque los array no tienen la " +
+					"misma longitud");
+		}
 	}
 	
 	public void setPedales (int numero)
